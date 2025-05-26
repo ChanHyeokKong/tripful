39f6fe0dad64497733402deaabda3d3cdab41fbf
ChanHyeokKong <kch6355@naver.com>
2025-05-26 17:30:38 +0900

    commit

---
+++ TripFul_Project/src/main/webapp/login/JavaScript/forgotJS.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+function findfunc() {
+
+	window.open("findID.jsp", "popup_findID", "width=400, height=200,scrollbars=no,resizable=no,status=no,top=300,left=800");
+
+	$("#findID").attr("target", "popup_findID").submit();
+
+}
+
+function findPass() {
+
+	window.open("findPW.jsp", "popup_findPW", "width=400, height=200,scrollbars=no,resizable=no,status=no,top=300,left=800");
+
+	$("#findID").attr("target", "popup_findID").submit();
+
+}
+
+function changePass(){
+	
+}
